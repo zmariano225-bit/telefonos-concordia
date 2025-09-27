6a6925c80fff4e12fecbe32378f1859b191fe3f0
zmariano225-bit <zmariano225@gmail.com>
2025-09-27 02:22:03 -0300

    Add files via upload

---
+++ sw.js.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'telefonos-concordia-v1';
+const urlsToCache = [
+  './',
+  'telefonos_utiles_concordia.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
